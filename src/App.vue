929a7c2358f5e05e90c0f6c41e829b1251abbdcb
ackermanjayjay <reza23198@gmail.com>
2024-01-27 15:59:39 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+import Navbar from "./components/Navbar.vue";
+</script>
+
+<template>
+  <Navbar></Navbar>
+  <HelloWorld></HelloWorld>
+</template>
+
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from "vue";
+
+const number1 = ref(0);
+const number2 = ref(0);
+
+</script>
+<template>
+  <div
+    class="grid grid-rows-4 grid-flow-col gap-4 justify-center mx-5 mb-5 mt-5"
+  >
+    <div>
+      <span class="badge">Input number here here</span>
+      <input
+        placeholder="Type here"
+        class="input input-bordered w-full max-w-xs"
+        v-model="number1"
+        type="text"
+      />
+    </div>
+    <div>
+      <input
+        placeholder="Type here"
+        class="input input-bordered w-full max-w-xs"
+        v-model="number2"
+        type="text"
+      />
+    </div>
+    <p>Result text is: {{ (+number1) + (+number2) }}</p>
+  </div>
+</template>
